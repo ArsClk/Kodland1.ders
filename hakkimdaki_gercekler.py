736dbeb723b42b23c76c5c4af55ddffe2ee59416
ArsClk <arascelik2007@gmail.com>
2024-08-09 13:00:03 +0300

    Create hakkimdaki_gercekler.py

---
+++ hakkimdaki_gercekler.py
@@ -0,0 +1,14 @@
+my_words = ["Hi!","Python Pro","Kodland"]
+print(my_words[1])
+
+#name = input()
+#print("Hi", name)
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+
+hakkimdaki_gercekler = ["Shakira","Köfte","Üniversite Son Sınıf Projem"]
+#import random
+hakkimdaki_gercek = random.choice(hakkimdaki_gercekler)
+print(hakkimdaki_gercek)
